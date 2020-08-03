84409fb94ded76947c9f7d53d9d44d81ed72c8d0
izzatjabali <izyjab@gmail.com>
2020-08-03 18:59:57 +1000

    Initial Commit

---
+++ Quizzy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizzy
+//
+//  Created by Izzat Jabali on 3/8/20.
+//  Copyright © 2020 Izzat Jabali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
